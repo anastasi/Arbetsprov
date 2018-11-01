bd7988fe3406ec64a97a599b00db6f54bb99be08
Anastasia Yanvareva <anastasiayanvareva@MAC-ANAYAN.local>
2018-11-01 15:21:21 +0100

    Styled History block

--- app/src/components/SearchHistory.js
+++ app/src/components/SearchHistory.js
@@ -5,9 +5,18 @@ const SearchHistory = ({ history, deleteFromHistory }) => {
   const historyList = history.length ? (history.map(item => {
     return(
       <li key={item.id} className="SearchHistoryItem" >
-        <div className="Container">{item.title}</div>
-        <span className="DeleteBtn" onClick={()=>{deleteFromHistory(item.id)}}><img src={Delete}/></span>
-      </li>
+  <div className="Container">
+      <div className="FlexContainer">
+        <div className="FlexItem">
+          {item.title}
+        </div>
+        <div className="FlexItem">
+          2018-02-22 11:00
+          <span className="DeleteBtn" onClick={()=>{deleteFromHistory(item.id)}}><img src={Delete}/></span>
+        </div>
+      </div>
+  </div>
+</li>
     )
   })) : null
   console.log('HiStOrY',history)
@@ -23,3 +32,4 @@ const SearchHistory = ({ history, deleteFromHistory }) => {
 }
 
 export default SearchHistory
+
